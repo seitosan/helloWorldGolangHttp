5d776ea181153ab6fc4ff52d1cf13414e094aba8
seitosan <seitosan@mac.home>
2026-02-10 01:17:05 +0100

    feat: upgrade new version

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
-}
+	// Création du routeur par défaut (avec logger et recovery middleware)
+	r := gin.Default()
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	// Définition de la route avec un paramètre nommé ":pattern"
+	r.GET("/:pattern", func(c *gin.Context) {
+		// Extraction du paramètre directement par son nom
+		pattern := c.Param("pattern")
+
+		// Réponse en texte brut (ou HTML)
+		c.String(http.StatusOK, "Hello, %s!", pattern)
+	})
+
+	// Lancement du serveur sur le port 8080
+	r.Run(":8080")
 }
